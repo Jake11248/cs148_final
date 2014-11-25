2c716d04e6df143d74bba12457ea833836a22c91
Jake11248 <jwarshaw@gmail.com>
2014-11-25 18:14:11 -0500

    Commit 11/25 6:15

--- assignment10/bicycle_safety.php
+++ assignment10/bicycle_safety.php
@@ -4,24 +4,36 @@
 include "top.php";
 ?>
 
-
-    <h3>
+<aside>
+    <br>
+    <br>
+    
+    <h3 id="safetyPageH3">
         The best way to prevent instances of being "buzzed" is to be seen
      </h3>
+    
+    <br>
+    <br>
 
-<img id= "carVsBike" src="https://jwarshaw.w3.uvm.edu/cs148/assignment10/bicyle_vs_car.jpg" alt="Bicycle vs Car Accident">
 <p>
+       <img id= "carVsBike" src="https://jwarshaw.w3.uvm.edu/cs148/assignment10/bicyle_vs_car.jpg" alt="Bicycle vs Car Accident">
+
    While it may be true that some of these buzzing issues come about because of 
    angry motorists. Many of the issues also come about because of the difficulty 
    of seeing a cyclist. So what can you do to be seen? 
+
 </p> 
 
 <h3>
         The simplest way to be seen is to wear neon clothes and always ride 
         with lights!
 </h3>
+<br>
+<br>
 
 <p>
+        <img id= "cyclingVest" src="https://jwarshaw.w3.uvm.edu/cs148/assignment10/cycling_vest.JPG" alt="Neon Cyclign Vest">
+
     Many cyclists think that neon clothes and lights are nudry. That wearing 
     neon clothes makes them look "less pro" then those who do not wear them. The 
     same feeling goes for lights. Many cyclists feel that lights are for "joeys".
@@ -34,11 +46,16 @@ include "top.php";
     they are essentially what they sound like, very bright bicycle clothes. Often 
     they come as a best so you can wear it anytime of the year. An example of such 
     a vest is below. You can find this type of cycling clothing at any bicycle shop. 
+
 </p>
 
-<img id= "cyclingVest" src="https://jwarshaw.w3.uvm.edu/cs148/assignment10/cycling_vest.JPG" alt="Neon Cyclign Vest">
+<br>
+<br>
 
 <p>
+    <img id= "bicycleLight" src="https://jwarshaw.w3.uvm.edu/cs148/assignment10/bicyle_light.jpg" 
+     alt="Neon Cyclign Vest">
+    
     So now that we know what to wear, now how about lights? Lights are incredibly
     helpful in being seen, especially at night. In fact, they are so helpful that 
     some municipalities now require cyclists to use light after dusk and before dawn. 
@@ -48,12 +65,11 @@ include "top.php";
     The best configuration for lights is to have a white light on the front and 
     a blinking red light on the back. Again, these lights can be found at any bicycle
     shop or online. 
+
 </p>
     
-<img id= "bicycleLight" src="https://jwarshaw.w3.uvm.edu/cs148/assignment10/bicyle_light.jpg" 
-     alt="Neon Cyclign Vest"
-     width= "400">
 
+</aside>
 
 <?php
 include "footer.php";
